e96ae9c31910f03927a54b854507ca5174e2e84d
zhangdada <937741304@qq.com>
2020-02-09 23:50:39 +0800

    更新

--- 复习js练习/1.js
+++ 复习js练习/1.js
@@ -1,5 +1,5 @@
-console.log('引入了我');
+// console.log('引入了我');
 
-document.write("写入内容"); // = "写入的内容";
+// document.write("写入内容"); // = "写入的内容";
 
 // document.body.innerHTML = '<h1>Hello World</h1>';
